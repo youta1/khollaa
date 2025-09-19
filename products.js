c51c0402dd8f44e203bc199c0c79c0796bb2016f
youta1 <samirdjouadi17@gmail.com>
2025-09-19 17:51:45 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,23 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/logo.png',
+		image: 'img/products/1.png',
+		image2: 'img/products/2.png',
+		image3: 'img/products/3.png',
+		image4: 'img/products/4.png',
+		image5: 'img/products/5.png',
+		title: '  مشروب الخلعةحصريا ',
+		title2: '  مشروب الخلعةحصريا ',
+		title: '  مشروب الخلعةحصريا ',
+		price1: 4800,
+		price: 2800,
+		description: '   '   ,
+		phone: 0770898634,
+	},
+
+
+	
+];
+
+  
